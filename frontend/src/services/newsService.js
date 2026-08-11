ed2e00453abab45ec2053aef65bcd465f79efff9
Devanshi <dubyedevanshi14@gmail.com>
2026-08-11 20:35:24 +0530

    Connect news API to deployed backend

--- frontend/src/services/newsService.js
+++ frontend/src/services/newsService.js
@@ -1,8 +1,15 @@
-import axios from "axios";
 
-const API = "/api/news";
+import axios from "axios";
+import API_URL from "./api";
+
+const API = `${API_URL}/api/news`;
+
+// ======================================================
+// GET FINANCE NEWS
+// ======================================================
 
 export const getNews = async () => {
   const response = await axios.get(API);
   return response.data;
 };
+
